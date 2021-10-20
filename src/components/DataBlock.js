e6d8acc5780111a5068a8e7c6bcbc921a0f04ff5
itata222 <72316879+itata222@users.noreply.github.com>
2021-10-20 22:17:22 +0300

    nearly done v2

--- src/components/DataBlock.js
+++ src/components/DataBlock.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const DataBlock = ({dataBlock,i}) => {
-    console.log(dataBlock)
-    //data block should consist url and depth
+const DataBlock = ({ dataBlock, i, key }) => {
+  //data block should consist url and depth
 
-    const dataBlockClicked=()=>{
-        console.log('data block clicked')
-    }
+  const dataBlockClicked = () => {
+    console.log("data block clicked");
+  };
 
-    return (
-        <div className="dataBlock" onClick={dataBlockClicked} style={{transform:`translatex(${i*5}px)`}}>
-           <span>{dataBlock.myAddress}</span>
-        </div>
-    )
-}
+  return (
+    <div className='dataBlock' onClick={dataBlockClicked} style={{ transform: `translatex(${i * 5}px)` }}>
+      <span>{dataBlock.parentAddress}</span>
+      <span>{dataBlock.position}</span>
+      <span>{dataBlock.myAddress}</span>
+    </div>
+  );
+};
 
-export default DataBlock
+export default DataBlock;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,77 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import { getCrawlingStatus, runCrawler } from '../services/crawlerService';
-import DataBlock from './DataBlock';
-import SummeryLine from './SummeryLine';
+import React, { useEffect, useState } from "react";
+import { getCrawlingStatus, runCrawler } from "../services/crawlerService";
+import DataBlock from "./DataBlock";
+import SummeryLine from "./SummeryLine";
 
 const Home = () => {
-    const [tree, setTree] = useState([]);
-    const [rootUrl, setRootUrl] = useState('');
-    const [maxDepth, setMaxDepth] = useState('');
-    const [maxTotalPages, setMaxTotalPages] = useState('');
-    const [QueueUrl, setQueueUrl] = useState('');
+  const [tree, setTree] = useState([]);
+  const [rootUrl, setRootUrl] = useState("");
+  const [maxDepth, setMaxDepth] = useState("");
+  const [maxTotalPages, setMaxTotalPages] = useState("");
+  const [QueueUrl, setQueueUrl] = useState("");
 
-    // const [currentDepth, setCurrentDepth] = useState('');
-    // const [currentChildrens, setCurrentChildrens] = useState('');
-    const [crawlerRunning, setCrawlerRunning] = useState(false);
-    
+  // const [currentDepth, setCurrentDepth] = useState('');
+  // const [currentChildrens, setCurrentChildrens] = useState('');
+  const [crawlerRunning, setCrawlerRunning] = useState(false);
 
-    const submitForm=(e)=>{
-        e.preventDefault();
-        if(parseInt(maxDepth)>0)
-            runCrawler({rootUrl,maxDepth,maxTotalPages}).then((res)=>{
-                if(!!res.QueueUrl){
-                    console.log('crwaler running!',res)
-                    setQueueUrl(res.QueueUrl)
-                    setCrawlerRunning(true)
-                }
-            }).catch(e=>console.log(e))
-    }
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (parseInt(maxDepth) > 0)
+      runCrawler({ rootUrl, maxDepth, maxTotalPages })
+        .then((res) => {
+          if (!!res.QueueUrl) {
+            console.log("crwaler running!", res);
+            setQueueUrl(res.QueueUrl);
+            setCrawlerRunning(true);
+          }
+        })
+        .catch((e) => console.log(e));
+  };
 
-    useEffect(() => {
-        if(crawlerRunning){
-            const intervalID= setInterval(() => {
-                console.log(crawlerRunning)
-                getCrawlingStatus(QueueUrl).then(res=>{
-                    console.log('checking',res)
-                    if(!!res?.tree){
-                        console.log(res.tree)
-                        clearInterval(intervalID)
-                        for(let i=0;i< res.tree.length;i++)
-                           res.tree[i]=JSON.parse(res.tree[i])
-                        setTree(res.tree)
-                        setCrawlerRunning(false)
-                        // setCurrentDepth(res.currentDepth||1)
-                        // setCurrentChildrens(res.currentChildrens||20)
-                    }
-                }).catch(e=>console.log(e))
-            }, 5000);
-        }
-    }, [crawlerRunning,QueueUrl]);
-    
-    
-    return (
-        <div className="home">
-            <h1>Crawler</h1>
-            <form onSubmit={submitForm} className="crawler-form">
-                <input disabled={!!crawlerRunning} type="text" placeholder="root url" value={rootUrl} onChange={(e)=>setRootUrl(e.target.value)}/>
-                <input className={parseInt(maxDepth)<1?'input-error':''} disabled={!!crawlerRunning} type="number" name="maxDepth" placeholder="max depth" value={maxDepth} onChange={(e)=>setMaxDepth(e.target.value)}/>
-                <input className={parseInt(maxTotalPages)<1?'input-error':''} disabled={!!crawlerRunning} type="number" name="maxTotalPages" placeholder="max total pages" value={maxTotalPages} onChange={(e)=>setMaxTotalPages(e.target.value)}/>
-                <button disabled={crawlerRunning}>Run Worker !</button>
-            </form>
-            {
-                tree.length>0&&
-                <>
-                    <SummeryLine rootUrl={rootUrl} depth={tree[tree.length-1].depth} childrens={tree.length}/>
-                    {
-                        tree.map((node,i)=>(
-                            <DataBlock key={node} dataBlock={node} i={i}/>
-                        ))
-                    }
-                </>
-                
+  useEffect(() => {
+    if (crawlerRunning) {
+      const intervalID = setInterval(() => {
+        console.log(crawlerRunning);
+        getCrawlingStatus(QueueUrl)
+          .then((res) => {
+            console.log("checking", res);
+            if (!!res?.tree) {
+              clearInterval(intervalID);
+              for (let i = 0; i < res.tree.length; i++) res.tree[i] = JSON.parse(res.tree[i]);
+              const sortedTree = res.tree.sort(function (a, b) {
+                return a.position - b.position;
+              });
+              console.log(sortedTree);
+              setTree(sortedTree);
+              setCrawlerRunning(false);
+              // setCurrentDepth(res.currentDepth||1)
+              // setCurrentChildrens(res.currentChildrens||20)
             }
-        </div>
-    )
-}
+          })
+          .catch((e) => console.log(e));
+      }, 5000);
+    }
+  }, [crawlerRunning, QueueUrl, maxTotalPages]);
 
-export default Home
+  return (
+    <div className='home'>
+      <h1>Crawler</h1>
+      <form onSubmit={submitForm} className='crawler-form'>
+        <input disabled={!!crawlerRunning} type='text' placeholder='root url' value={rootUrl} onChange={(e) => setRootUrl(e.target.value)} />
+        <input
+          className={parseInt(maxDepth) < 1 ? "input-error" : ""}
+          disabled={!!crawlerRunning}
+          type='number'
+          name='maxDepth'
+          placeholder='max depth'
+          value={maxDepth}
+          onChange={(e) => setMaxDepth(e.target.value)}
+        />
+        <input
+          className={parseInt(maxTotalPages) < 1 ? "input-error" : ""}
+          disabled={!!crawlerRunning}
+          type='number'
+          name='maxTotalPages'
+          placeholder='max total pages'
+          value={maxTotalPages}
+          onChange={(e) => setMaxTotalPages(e.target.value)}
+        />
+        <button disabled={crawlerRunning}>Run Worker !</button>
+      </form>
+      {tree.length > 0 && (
+        <>
+          <SummeryLine rootUrl={rootUrl} depth={tree[tree.length - 1].depth} childrens={tree.length} />
+          {tree.map((node, i) => (
+            <div key={`${node.rootUrl}$${node.depth}$${node.position}`}>
+              <DataBlock dataBlock={node} i={i} />
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
